b1c3cc08159d522f9ce09908dca17963784eb04a
Egor Antonov <antonovegorv@gmail.com>
2022-03-18 15:47:42 +0300

    refactor task 25

--- dev25/main.go
+++ dev25/main.go
@@ -1,4 +1,4 @@
-// Task 25
+// 25
 // Реализовать собственную функцию sleep.
 
 package main
@@ -9,6 +9,7 @@ func main() {
 	sleep(2 * time.Second)
 }
 
+// sleep — blocks main goroutine until time.After returns a channel.
 func sleep(d time.Duration) {
 	<-time.After(d)
 }
