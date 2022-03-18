48d00dd0f25b2aa36f65de0f94ee5244b416de20
Egor Antonov <antonovegorv@gmail.com>
2022-03-18 14:52:04 +0300

    refactor task 08

---
+++ dev08/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(toggleBit(0, 0)) // Outputs: 1 true
+}
+
+// toggleBit — toggles i'th bit by XORing n with 1 left shifted by i. Second return value signals
+// about operation success.
+func toggleBit(n int64, i uint) (int64, bool) {
+	// Check whether i is greater than 63
+	if i > 63 {
+		return n, false
+	}
+	return n ^ (1 << i), true // Here we xor n with 1 left shifted by i
+}
